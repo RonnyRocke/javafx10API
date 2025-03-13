4d75484271221852ff1dc69fd8ae9bd87d51d457
RonnyRocke <raimundolindocarneiro@gmail.com>
2025-03-13 02:47:12 -0300

    first commit

---
+++ src/main/java/dev/java10fx_CadastroDeNinjas/Java10fxCadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10fx_CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java10fxCadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java10fxCadastroDeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10fx_CadastroDeNinjas/Java10fxCadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10fx_CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java10fxCadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
